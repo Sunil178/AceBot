894c7fe4126717e0fc1f0d80719cedc183f707a1
Sunil Thakur <sunilthakur178co@gmail.com>
2022-11-27 19:05:55 +0530

    Added db file

--- db.php
+++ db.php
@@ -2,7 +2,6 @@
 
 	$db_host = "127.0.0.1";
 	$db_user = "root";
-	$db_pass = "1234";
+	$db_pass = "sunil@1234";
 	$db_name = "AceBot";
-
 	$conn = mysqli_connect("$db_host", "$db_user", "$db_pass", "$db_name") or die ("Could not connect to MySQL");
